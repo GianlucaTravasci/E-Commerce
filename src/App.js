33d4df89cd1427e9c04b4702ae6e58ee63e546b1
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-08 10:03:44 +0200

    add the user authentication and signin with google

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Route, Switch } from 'react-router-dom'
 import './App.css';
 
@@ -6,25 +6,47 @@ import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup'
+import {auth} from './firebase/firebase.util'
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/shop">
-          <ShopPage />
-        </Route>
-        <Route path="/signin">
-          <SignInAndSignUpPage />
-        </Route>
-      </Switch>
-    </div>
-  );
+  unsubscribeFromAuth=null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/shop">
+            <ShopPage />
+          </Route>
+          <Route path="/signin">
+            <SignInAndSignUpPage />
+          </Route>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
