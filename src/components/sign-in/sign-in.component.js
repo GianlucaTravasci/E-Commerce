401291e520d7170ee320b5569d933be094911802
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-08 17:41:23 +0200

    simple fix for the signin/up view

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -51,7 +51,7 @@ class SignIn extends Component {
 
                     <div className="buttons">
                         <CustomButton type="submit">Sign in</CustomButton>
-                        <CustomButton isGoogleSignin onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+                        <CustomButton isGoogleSignin onClick={signInWithGoogle} type="button">Sign in with Google</CustomButton>
                     </div>
                 </form> 
             </div>
