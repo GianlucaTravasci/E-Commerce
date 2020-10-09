f423706c71479f69d6e92cb77ec8f2237bcd57a4
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-09 17:21:46 +0200

    more on redux and changed the state handling in the header component

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import {auth} from '../../firebase/firebase.util'
 
 import './header.styles.scss'
+
+const mapStateToProps = (state) => ({
+    currentUser: state.user.currentUser
+})
+
 
 const Header = ({ currentUser }) => {
     return (
@@ -23,4 +29,6 @@ const Header = ({ currentUser }) => {
     )
 }
 
-export default Header;
+
+
+export default connect(mapStateToProps)(Header);
