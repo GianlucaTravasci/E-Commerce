da1e717991bed82b8e6d9dd3dee792e01933d3b6
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-10 15:29:59 +0200

    typo fix

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,6 @@
 export const addItemToCart = (cartItem, cartItemToAdd) => {
-    const existingCartITem=cartItem.find(cartItem => cartItem.id === cartItemToAdd.id)
-    if (existingCartITem) {
+    const existingCartItem=cartItem.find(cartItem => cartItem.id === cartItemToAdd.id)
+    if (existingCartItem) {
         return cartItem.map(cartItem => 
             cartItem.id === cartItemToAdd.id ? {...cartItem, quantity: cartItem.quantity +1 } : cartItem
         ) 
