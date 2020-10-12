046b5a87342486398d6ffe073343e563f571922e
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-10-12 10:21:19 +0200

    add local storage to the app for the cart section with redux persistance. User section has his local storage from Firebase

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux'
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import {store, persitor} from './redux/store';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import store from './redux/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate persistor={persitor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
